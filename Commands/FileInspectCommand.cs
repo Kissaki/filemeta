4a4b446d55ef4ea5966774b5c35ade4065ade6e7
Jan Klass <kissaki@posteo.de>
2024-06-22 20:29:51 +0200

    refac: Replace System.CommandLine with Spectre.Console

---
+++ Commands/FileInspectCommand.cs
@@ -0,0 +1,25 @@
+﻿using Spectre.Console;
+using static filemeta.Commands.FileInspectCommand;
+
+namespace filemeta.Commands
+{
+    internal sealed class FileInspectCommand : AsyncCommand<Settings>
+    {
+        public sealed class Settings : CommandSettings
+        {
+            [CommandArgument(position: 0, template: "<filepath>")]
+            public required FileInfo FileInfo { get; set; }
+
+            public override ValidationResult Validate()
+            {
+                return FileInfo.Exists ? ValidationResult.Success() : ValidationResult.Error("File does not exist");
+            }
+        }
+
+        public override Task<int> ExecuteAsync(CommandContext context, Settings settings)
+        {
+            var err = FileInspector.OpenFileInfo(settings.FileInfo, null);
+            return Task.FromResult(err ? 1 : 0);
+        }
+    }
+}
---
+++ FileInspector.cs
@@ -0,0 +1,79 @@
+﻿namespace filemeta
+{
+    internal static class FileInspector
+    {
+        /// <returns>success</returns>
+        public static bool OpenFileInfo(FileInfo fi, CancellationToken? cToken1)
+        {
+            if (fi.IsReadOnly)
+            {
+                WriteFileMeta(fi);
+                Console.WriteLine("File is read-only. Metadata can not be changed.");
+                return false;
+            }
+
+            using var cTokenSource = new CancellationTokenSource();
+            var cToken = cTokenSource.Token;
+
+            while (!(cToken1?.IsCancellationRequested ?? false) || !cToken.IsCancellationRequested)
+            {
+                WriteFileMeta(fi);
+                Console.WriteLine("[q]uit");
+                Console.WriteLine("Use the marked keys to navigate...");
+                var c = Console.ReadKey();
+                Console.WriteLine();
+
+                switch (c.KeyChar)
+                {
+                    case 'q':
+                        cTokenSource.Cancel();
+                        break;
+                    case 'c':
+                        ChangeProperty(fi, x => x.CreationTime, (x, v) => x.CreationTime = v);
+                        break;
+                    case 'a':
+                        ChangeProperty(fi, x => x.LastAccessTime, (x, v) => x.LastAccessTime = v);
+                        break;
+                    case 'w':
+                        ChangeProperty(fi, x => x.LastWriteTime, (x, v) => x.LastWriteTime = v);
+                        break;
+                    default:
+                        // Break out into interactivity loop
+                        break;
+                }
+            }
+
+            return true;
+        }
+
+        public static void WriteFileMeta(FileInfo fi)
+        {
+            Console.WriteLine($"File {fi.Name}:");
+            Console.WriteLine($"  [C]reationTime  : {fi.CreationTime}");
+            Console.WriteLine($"  Last[A]ccessTime: {fi.LastAccessTime}");
+            Console.WriteLine($"  Last[W]riteTime : {fi.LastWriteTime}");
+            Console.WriteLine($"  Information     :");
+            Console.WriteLine($"    LinkTarget  : {fi.LinkTarget}");
+            Console.WriteLine($"    IsReadOnly  : {fi.IsReadOnly}");
+            Console.WriteLine($"    Length      : {fi.Length}");
+            Console.WriteLine($"    UnixFileMode: {fi.UnixFileMode}");
+        }
+
+        public static void ChangeProperty(FileInfo fi, Func<FileInfo, DateTime> get, Action<FileInfo, DateTime> set)
+        {
+            var dtFormat = "yyyy-MM-dd HH:mm:ss";
+            Console.WriteLine(get(fi).ToString(dtFormat));
+
+            var line = Console.ReadLine();
+            if (line == null) return;
+
+            if (!DateTime.TryParseExact(line, dtFormat, provider: CultureInfo.InvariantCulture, DateTimeStyles.None, out var newValue))
+            {
+                Console.WriteLine("Input could not be read as a date and time. Invalid format?");
+                return;
+            }
+
+            set(fi, newValue);
+        }
+    }
+}
